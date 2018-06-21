27174f982ff69ef55f9eb3ef60f9d7a28a83154b
Krysta05 <martinezkrysta7@gmail.com>
2018-06-21 18:29:18 +0000

    Done.

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,40 @@
-#write out your code here
+coins = {}
 
 def least_coins(cents)
 
-#Code your answer here!
+#46/25 = 1 r 21
+#21/10 = 2 r 1
+#1/5 = 0 r 1 
+#1/1 = 0 r 0
 
-end
+change={}
+
+quarters = cents/25 
+ puts quarters 
+quartersR = cents%25 
+ puts quartersR
+dimes = quartersR/10
+ puts dimes 
+dimesR = quartersR%10
+ puts dimesR 
+nickels = dimesR/5
+ puts nickels
+nickelsR = dimesR%5 
+ puts nickelsR
+pennies = nickelsR/1 
+ puts pennies
+penniesR = nickelsR%1 
+ puts penniesR
+ 
+ change["quarters"] =quarters
+ puts change
+change["dimes"] =dimes
+ puts change
+change["nickels"] =nickels
+ puts change
+change["pennies"] =pennies 
+ puts change
+ 
+end 
+
+least_coins (87)
